aae17d0134d5c8f9bccb0316f5b68200a0cc76f2
sabamosleh <saba_mosleh1996@yahoo.com>
2019-08-23 18:45:37 +0430

    singly linked list example

---
+++ src/main/java/linkedList/singlyLinkedList/LinkedList.java
@@ -0,0 +1,14 @@
+package linkedList.singlyLinkedList;
+
+public class LinkedList {
+
+    public void addNodeToFirst(Node node){
+
+
+
+
+    }
+
+
+
+}
---
+++ src/main/java/linkedList/singlyLinkedList/Node.java
@@ -0,0 +1,39 @@
+package linkedList.singlyLinkedList;
+
+public class Node {
+
+    private Node next;
+    private Node prev;
+    private int value;
+
+
+    public Node(Node next, Node prev, int value) {
+        this.next = next;
+        this.prev = prev;
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}

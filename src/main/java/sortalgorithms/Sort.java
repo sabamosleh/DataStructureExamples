72bfeefd2f62ad479533c5381f2b12e5aadc5aad
Navin Nayak <37023798+NayakNavin@users.noreply.github.com>
2019-10-08 12:54:26 +0530

    added merge sort

--- src/main/java/sortalgorithms/Sort.java
+++ src/main/java/sortalgorithms/Sort.java
@@ -59,6 +59,79 @@ public class Sort {
     }
 
 
+    /* Java program for Merge Sort */
+    // Merges two subarrays of inputArray[];
+    // First subarray is inputArray[l..m]
+    // Second subarray is inputArray[m+1..r]
+    
+    public int[] merge(int[] inputArray, int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = inputArray[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = inputArray[m + 1 + j];
+
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                inputArray[k] = L[i];
+                i++;
+            } else {
+                inputArray[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            inputArray[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            inputArray[k] = R[j];
+            j++;
+            k++;
+        }
+        return inputArray;
+
+    }
+
+
+    // Main function that sorts inputArray[l..r] using
+    // merge()
+    void sort(int[] inputArray, int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = (l + r) / 2;
+
+            // Sort first and second halves
+            sort(inputArray, l, m);
+            sort(inputArray, m + 1, r);
+
+            // Merge the sorted halves
+            merge(inputArray, l, m, r);
+        }
+    }
+
     public Sort(int[] inputArray) {
         this.inputArray = inputArray;
     }
@@ -70,4 +143,6 @@ public class Sort {
     public void setInputArray(int[] inputArray) {
         this.inputArray = inputArray;
     }
+
+
 }

f092adf6982e73a23557402ffbfa2acea54afa17
Navin Nayak <37023798+NayakNavin@users.noreply.github.com>
2019-10-08 12:34:26 +0530

    Update Test.java

--- src/main/java/sortalgorithms/Test.java
+++ src/main/java/sortalgorithms/Test.java
@@ -11,8 +11,7 @@ public class Test {
 
         Sort doSort=new Sort(inputs);
         System.out.println(Arrays.toString(doSort.bubbleSort(inputs)));
-
-
+        System.out.println(Arrays.toString(doSort.selectionSort(inputs)));
 
 
 

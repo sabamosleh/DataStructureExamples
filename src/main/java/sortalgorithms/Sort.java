f06a702fe8bba0e79d732c2503ab31a0ca566a05
Akhil09 <sharmaakb12@gmail.com>
2019-10-08 13:01:45 +0530

    Added merge sort in Sort class

--- src/main/java/sortalgorithms/Sort.java
+++ src/main/java/sortalgorithms/Sort.java
@@ -34,6 +34,64 @@ public class Sort {
         return inputArray;
 
     }
+    
+    
+	private void merge(int arr[], int l, int m, int r) 
+	{ 
+		int n1 = m - l + 1; 
+		int n2 = r - m; 
+
+		int L[] = new int [n1]; 
+		int R[] = new int [n2]; 
+
+		for (int i=0; i<n1; ++i) 
+			L[i] = arr[l + i]; 
+		for (int j=0; j<n2; ++j) 
+			R[j] = arr[m + 1+ j]; 
+
+
+		{ 
+			if (L[i] <= R[j]) 
+			{ 
+				arr[k] = L[i]; 
+				i++; 
+			} 
+			else
+			{ 
+				arr[k] = R[j]; 
+				j++; 
+			} 
+			k++; 
+		} 
+
+		{ 
+			arr[k] = L[i]; 
+			i++; 
+			k++; 
+		} 
+
+		while (j < n2) 
+		{ 
+			arr[k] = R[j]; 
+			j++; 
+			k++; 
+		} 
+	} 
+    
+	public int[] mergeSort(int[] inputArray, int l, int r) 
+	{ 
+		if (l < r) 
+		{ 
+			int m = (l+r)/2; 
+
+			mergeSort(inputArray, l, m); 
+			mergeSort(inputArray , m+1, r); 
+
+			merge(inputArray, l, m, r); 
+		}
+        return inputArray;
+	} 
+
 
     public Sort(int[] inputArray) {
         this.inputArray = inputArray;

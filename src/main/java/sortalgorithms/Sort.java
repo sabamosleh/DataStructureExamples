bb9a99df1def0cdfeb5abd824536ef9c884199e5
Navin Nayak <37023798+NayakNavin@users.noreply.github.com>
2019-10-08 12:24:52 +0530

    Update Sort.java

--- src/main/java/sortalgorithms/Sort.java
+++ src/main/java/sortalgorithms/Sort.java
@@ -6,23 +6,23 @@ public class Sort {
     private int[] inputArray;
 
 
-    public int[] bubbleSort(int[] inputArray){
+    public int[] bubbleSort(int[] inputArray) {
 
-        int size=inputArray.length-1;
-        int temp=-1;
+        int size = inputArray.length - 1;
+        int temp = -1;
 
-        if(size==1)
+        if (size == 1)
             return inputArray;
 
-        for(int i=0;i<=size;i++){
+        for (int i = 0; i <= size; i++) {
 
-            for(int j=0;j<size-i;j++){
+            for (int j = 0; j < size - i; j++) {
 
-                if(inputArray[j]>inputArray[j+1]){
+                if (inputArray[j] > inputArray[j + 1]) {
 
-                    temp=inputArray[j+1];
-                    inputArray[j+1]=inputArray[j];
-                    inputArray[j]=temp;
+                    temp = inputArray[j + 1];
+                    inputArray[j + 1] = inputArray[j];
+                    inputArray[j] = temp;
 
 
                 }
@@ -34,6 +34,30 @@ public class Sort {
         return inputArray;
 
     }
+
+
+    public int[] selectionSort(int[] inputArray) {
+
+        int size = inputArray.length - 1;
+
+        if (size == 1)
+            return inputArray;
+
+        for (int i = 0; i < size; i++) {
+            int index = i;
+            for (int j = i + 1; j < size; j++) {
+                if (inputArray[j] < inputArray[index]) {
+                    index = j;//searching for lowest index
+                }
+            }
+            int smallerNumber = inputArray[index];
+            inputArray[index] = inputArray[i];
+            inputArray[i] = smallerNumber;
+        }
+        return inputArray;
+
+    }
+
 
     public Sort(int[] inputArray) {
         this.inputArray = inputArray;

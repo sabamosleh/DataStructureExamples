5c96ff8c8e41862c48afae80dcf5e25a25f6bf50
Doni Aprilianto <doni.aprilianto@inmotion.co.id>
2019-10-17 18:12:07 +0700

    add insertion sort and simple test in Test class

--- src/main/java/sortalgorithms/Sort.java
+++ src/main/java/sortalgorithms/Sort.java
@@ -46,4 +46,18 @@ public class Sort {
     public void setInputArray(int[] inputArray) {
         this.inputArray = inputArray;
     }
+
+    public int[] insertionSort(){
+      int n = inputArray.length;
+      for (int i = 1; i < n; ++i) {
+          int key = inputArray[i];
+          int j = i - 1;
+          while (j >= 0 && inputArray[j] > key) {
+              inputArray[j + 1] = inputArray[j];
+              j = j - 1;
+          }
+          inputArray[j + 1] = key;
+      }
+      return inputArray;
+    }
 }
--- src/main/java/sortalgorithms/Test.java
+++ src/main/java/sortalgorithms/Test.java
@@ -3,19 +3,12 @@ package sortalgorithms;
 import java.util.Arrays;
 
 public class Test {
+  public static void main(String[] args) {
+        int[] inputs = new int[]{90,32,19,18,4,1};
 
-    public static void main(String[] args) {
-
-        int[] inputs=new int[]{90,32,19,18,4,1};
-
-
-        Sort doSort=new Sort(inputs);
+        Sort doSort = new Sort(inputs);
         System.out.println(Arrays.toString(doSort.bubbleSort(inputs)));
 
-
-
-
-
+        System.out.println(Arrays.toString(doSort.insertionSort()));
     }
-
 }

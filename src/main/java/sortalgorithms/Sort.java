136e73f2ce67cd4a095a0cc179f9b3e57d9e0579
saba.mosleh <saba_mosleh1996@yahoo.com>
2019-11-15 00:52:55 +0330

    add quick sort.

--- src/main/java/sortalgorithms/Sort.java
+++ src/main/java/sortalgorithms/Sort.java
@@ -6,6 +6,19 @@ public class Sort {
     private int[] inputArray;
 
 
+    public int[] getInputArray() {
+        return inputArray;
+    }
+
+    public void setInputArray(int[] inputArray) {
+        this.inputArray = inputArray;
+    }
+
+    public Sort(int[] inputArray) {
+        this.inputArray = inputArray;
+    }
+
+
     public int[] bubbleSort(int[] inputArray){
 
         int size=inputArray.length-1;
@@ -56,15 +69,48 @@ public class Sort {
 
     }
 
-    public Sort(int[] inputArray) {
-        this.inputArray = inputArray;
+    //i use this method in quick sort to bring the pivot to the middle of the array
+    //and set up less elements in left side and right elements to the right side of
+    //the pivot element
+    public int partition(int[] inputArray,int start,int end){
+
+        int pivot=inputArray[end];
+
+        int i=start-1;
+        for(int j=start;j<end;j++){
+
+            if(inputArray[j]<=pivot){
+
+                i++;
+                int temp=inputArray[j];
+                inputArray[j]=inputArray[i];
+                inputArray[i]=temp;
+
+            }
+
+
+        }
+
+       int temp=inputArray[i+1];
+        inputArray[i+1]=inputArray[end];
+        inputArray[end]=temp;
+
+        return i+1;
+
     }
 
-    public int[] getInputArray() {
+
+    public int[] quickSort(int[] inputArray,int start,int end){
+
+        if(start<end){
+
+            int pivot=partition(inputArray,start,end);
+            quickSort(inputArray,start,pivot-1);
+            quickSort(inputArray,pivot+1,end);
+        }
         return inputArray;
-    }
 
-    public void setInputArray(int[] inputArray) {
-        this.inputArray = inputArray;
+
+
     }
 }
